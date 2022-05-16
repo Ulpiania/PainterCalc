b4e5520d91f9069a7a14bc8b80d92148883d390d
Alex Ulpiani <alexulp@hotmail.com>
2022-05-16 17:15:53 +0100

    Added coats and total cost functionality

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,9 +5,8 @@ public class Calculator {
     public static void main(String[] args){
 
         int numberOfWalls = 0;
-        float bucketAmount, wallWidth, wallHeight,totalWidth, totalHeight, finalSize, bucketsNeeded;
-        float wallArea = 0;
-        float extraArea = 0;
+        int coats =1;
+        float bucketAmount, wallWidth, wallHeight,totalWidth, totalHeight, finalSize, bucketsNeeded, wallArea, extraArea, bucketCost, totalPrice;
         float finalWallArea = 0;
         float finalExtraArea = 0;
         Scanner myScanner = new Scanner(System.in);
@@ -24,16 +23,16 @@ public class Calculator {
         float[] extraAreas = new float[numberOfWalls];
         while (numberOfWalls > 0) {
             System.out.println("Wall number: " + numberOfWalls);
-            System.out.println("What is the width of the wall?");
+            System.out.println("What is the width of the wall in meters?");
             wallWidth = Float.parseFloat(myScanner.nextLine());
-            System.out.println("What is the height of the wall?");
+            System.out.println("What is the height of the wall in meters?");
             wallHeight = Float.parseFloat(myScanner.nextLine());
 
 
-            System.out.println("What is the total width of any extra objects on the wall such as Doors/Sockets/Windows (0 if N/A)");
+            System.out.println("What is the total width in meters of any extra objects on the wall such as Doors/Sockets/Windows (0 if N/A)");
             totalWidth = Float.parseFloat(myScanner.nextLine());
 
-            System.out.println("What is the total height of any extra objects on the wall such as Doors/Sockets/Windows (0 if N/A)");
+            System.out.println("What is the total height in meters of any extra objects on the wall such as Doors/Sockets/Windows (0 if N/A)");
             totalHeight = Float.parseFloat(myScanner.nextLine());
 
             int noOfWalls = numberOfWalls;
@@ -57,12 +56,18 @@ public class Calculator {
             finalExtraArea += totalWidth * totalHeight;
 
         }
-        System.out.println("How much square meters can the amount of paint in your bucket cover?");
+        System.out.println("How many square meters can the amount of paint in your bucket cover?");
         bucketAmount = Float.parseFloat(myScanner.nextLine());
+        System.out.println("How many coats of paint do you want to use?");
+        coats = Integer.parseInt(myScanner.nextLine());
+        System.out.println("How much in pounds does a single bucket cost?");
+        bucketCost = Integer.parseInt(myScanner.nextLine());
         finalSize = finalWallArea - finalExtraArea;
-        bucketsNeeded = (float) Math.ceil(finalSize / bucketAmount);
+        bucketsNeeded = (float) Math.ceil(finalSize / bucketAmount) * coats;
+        totalPrice = bucketsNeeded * bucketCost;
 
         System.out.println("The final total surface area of all the walls after removing the extra objects is: " + finalSize);
-        System.out.println("You will need: " + bucketsNeeded + " buckets to fully paint the wall.");
+        System.out.println("You will need: " + bucketsNeeded + " buckets to fully paint the wall with " + coats + " coats.");
+        System.out.println("It will cost: " + totalPrice + " pounds in total");
     }
 }
